c3bd77d063a924d89675af30a1c35c619ee0d276
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-02-09 13:00:05 -0700

    URLs now mapped to categories and connected with pages. End of Ch. 7

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
 	category_list=Category.objects.order_by('-likes')[:5]
@@ -10,3 +11,19 @@ def index(request):
 
 
 	return render(request, 'rango/index.html', context_dict)
+
+def category(request, category_name_slug):
+	context_dict ={}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name']=category.name
+
+		pages = Page.objects.filter(category=category)
+
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+
+	return render(request, 'rango/category.html', context_dict)
