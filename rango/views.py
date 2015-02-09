777e54f2bc3ac8c486e76c9bea4c71cc1bccc71c
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-02-09 14:37:24 -0700

    create a new category form and mappings

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
 	category_list=Category.objects.order_by('-likes')[:5]
@@ -27,3 +28,18 @@ def category(request, category_name_slug):
 		pass
 
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	#handle a POST request
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print form.errors
+	else:
+		form = CategoryForm()
+
+	return render(request, 'rango/add_category.html', {'form': form})
