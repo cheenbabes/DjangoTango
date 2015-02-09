5f2e047a2827122e410fd3562a4235197b0408fa
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-02-08 17:36:42 -0700

    changed project to Django 1.7

---
+++ tango_with_django_project/settings.py
@@ -0,0 +1,100 @@
+"""
+Django settings for tango_with_django_project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+TEMPLATE_PATH = os.path.join(SITE_ROOT, 'templates') 
+STATIC_PATH = os.path.join(SITE_ROOT, 'static')
+
+# DATABASE_PATH = os.path.join(SITE_ROOT, 'rango.db')
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '89skw%b($qrq9jx*4!6d%@6n_s(4(+!cgp*nv#+*99yblojkp*'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+#set to 127.0.0.1 if debug =false
+ALLOWED_HOSTS = []
+
+TEMPLATE_DIRS= [
+    TEMPLATE_PATH,
+]
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rango',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'tango_with_django_project.urls'
+
+WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS =(
+    STATIC_PATH,
+    )
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
